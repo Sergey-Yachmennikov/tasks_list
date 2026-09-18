9b9043b3294c9e1ae9476801ad217605772202d7
Sergey-Yachmennikov <otsmmm2013@gmail.com>
2026-09-18 19:45:34 +0300

    removed targes folders

--- kyc-verification-service/target/classes/db/migration/V1__init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE application (
-    id              UUID PRIMARY KEY,
-    client_id       UUID        NOT NULL,
-    status          VARCHAR(32) NOT NULL,
-    kyc_session_id  VARCHAR(128)
-);
